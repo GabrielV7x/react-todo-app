2792d345d9313d2d1a82ec71ea2c601838eaa6e2
Frederic Feliciane <fredericfeliciane8@gmail.com>
2025-05-01 22:49:39 +0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import ListSelector from "./components/ListSelector";
 import EditListTitle from "./components/EditListTitle";
 import TodoList from "./components/TodoList";
 
-// test
-
 const initialLists = [
   {
     id: 1,
